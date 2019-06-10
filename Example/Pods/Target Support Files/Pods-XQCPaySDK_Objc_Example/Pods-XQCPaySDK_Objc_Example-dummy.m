fdb911d8d8b9a0dacd08bc5dacbc7e575b086ce4
lee <876231865@qq.com>
2019-06-10 18:04:51 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-XQCPaySDK_Objc_Example/Pods-XQCPaySDK_Objc_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XQCPaySDK_Objc_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XQCPaySDK_Objc_Example
+@end
---
+++ Example/Pods/Target Support Files/XQCPaySDK_Objc/XQCPaySDK_Objc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XQCPaySDK_Objc : NSObject
+@end
+@implementation PodsDummy_XQCPaySDK_Objc
+@end
